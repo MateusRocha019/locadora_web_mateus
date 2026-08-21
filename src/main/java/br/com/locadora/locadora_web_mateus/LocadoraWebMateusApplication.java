4cbd9ebef948f63b9fc5f24226577853466190d3
Mateus Rocha <mateusrochasoares100@gmail.com>
2026-08-21 20:49:42 -0300

    Configuração Projeto

---
+++ src/main/java/br/com/locadora/locadora_web_mateus/LocadoraWebMateusApplication.java
@@ -0,0 +1,13 @@
+package br.com.locadora.locadora_web_mateus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoraWebMateusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocadoraWebMateusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/locadora/locadora_web_mateus/LocadoraWebMateusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.locadora.locadora_web_mateus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraWebMateusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
